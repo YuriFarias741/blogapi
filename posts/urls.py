0eceb0186df54416e6f1acd043c613f5dce6984c
Yuri Farias <yuricoelhodefarias@hotmail.com>
2018-11-20 20:23:22 -0200

    First commit

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.routers import SimpleRouter
+
+from .views import PostViewSet, UserViewSet
+
+router = SimpleRouter()
+router.register('users', UserViewSet, base_name='users')
+router.register('', PostViewSet, base_name='posts')
+
+urlpatterns = router.urls
